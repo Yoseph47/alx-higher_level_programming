4b26360e445bbc749062fc2c85e8b60d7e5a6bf7
Yoseph47 <jossyseyoum@gmail.com>
2022-10-26 05:24:19 +0300

    input

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,14 +1,16 @@
-"""write a class student"""
+#!/usr/bin/python3
+
+""""
+ class Student that defines a student
+"""
 
 
 class Student:
-    """class student"""
+    """defines a student"""
     def __init__(self, first_name, last_name, age):
-        """initialization"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """creates a dict"""
         return self.__dict__
