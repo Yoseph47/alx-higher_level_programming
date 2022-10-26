0c4d47fb75605289ded0f8ef86c6622ea06e93b1
Yoseph47 <jossyseyoum@gmail.com>
2022-10-26 05:34:28 +0300

    class student based on 10

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python3
-"""write a class student"""
+
+""""
+ class Student that defines a student
+"""
 
 
 class Student:
-    """class student"""
+    """defines a student"""
     def __init__(self, first_name, last_name, age):
-        """initialization"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """creates a dict"""
-        if (type(attrs) == list and
-                all(type(e) == str for e in attrs)):
-            return {i: getattr(self,i) for i in attrs if hasattr(self, i)}
-        return self.__dict__
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+
+        for key in attrs:
+            try:
+                new_dict[key] = self.__dict__[key]
+            except Exception:
+                pass
+
+        return new_dict
 
     def reload_from_json(self, json):
-        """Replace all attributes of the student"""
-        for k, v in json.items():
-            setattr(self, k, v)
-            
+        for key in json:
+            try:
+                setattr(self, key, json[key])
+            except Exception:
+                pass 
